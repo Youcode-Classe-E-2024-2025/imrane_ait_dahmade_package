666a0a56be03b5a5003bc4618a92c2ae348580a7
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2024-12-11 15:10:55 +0100

    j ai cree le page html d  ajouter

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ajouter package</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="w-screen h-screen bg-green-200 flex justify-center">
+    <h1 class="text-3xl mt-14 font-Mont text-white">GESTION DES PACKAGES JSON</h1>
+    <form class=" w-1/4 h-[70%] bg-white absolute top-[20%]  rounded-lg shadow-sm shadow-green-400 flex flex-col gap-5 space-y-2 p-4 " action="">
+ 
+   <input class="w-1/2 h-[2rem]" type="text" placeholder="nom de package">
+   <input class="w-1/2 h-[2rem]" type="text" placeholder="descreption">
+   <input class="w-1/2 h-[2rem]" type="text" placeholder="auteur">
+   <input class="w-1/2 h-[2rem]" type="text" placeholder="email de l auteur">
+   <input class="w-1/2 h-[2rem]" type="text" placeholder="date de creation">
+   <button type="submit" class=" bg-green-500 h-[2rem] text-xl text-white rounded-lg hover:bg-green-700"> ajouter</button>
+
+   
+   
+
+
+    </form>
+
+
+
+</body>
+</html>
